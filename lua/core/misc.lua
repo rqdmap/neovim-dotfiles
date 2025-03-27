dc18bb8bcb256e848ae4c5c88006aa45bc13b989
rqdmap <55649208+rqdmap@users.noreply.github.com>
2025-03-28 01:50:20 +0800

    C 代码格式化方式调整

--- lua/core/misc.lua
+++ lua/core/misc.lua
@@ -1,7 +1,28 @@
--- local notify = vim.notify
+local notify = vim.notify
+
 -- vim.notify = function(msg, ...)
 --     if msg:match("vim.lsp.buf_get_clients() is deprecated. Run \":checkhealth vim.deprecated\" for more infor") then
 --         return
 --     end
 --     notify(msg, ...)
 -- end
+--
+
+local old_error = error
+_G.error = function(msg, level)
+  if type(msg) == "string" and msg:match("snippet parsing failed") then
+    return
+  end
+  old_error(msg, level)
+end
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+  pattern = "~/dream/linux/**/*",
+  callback = function()
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 8
+    vim.opt_local.expandtab = false
+  end
+})
+
--- lua/lang/c.lua
+++ lua/lang/c.lua
@@ -1,3 +1,4 @@
-vim.o.tabstop        = 8
-vim.o.shiftwidth     = 8
-vim.o.softtabstop    = 8
+-- 使用普通的 4 格缩进, 内核代码请单独设置
+vim.o.tabstop        = 4
+vim.o.shiftwidth     = 4
+vim.o.softtabstop    = 4
