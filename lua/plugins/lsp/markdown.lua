142d7922364321925bea8963e0a799e75bf5b546
rqdmap <55649208+rqdmap@users.noreply.github.com>
2025-12-11 10:51:14 +0800

    feat: 修改 markdown.preview 为手动安装的方式

--- lua/plugins/lsp/markdown.lua
+++ lua/plugins/lsp/markdown.lua
@@ -1,9 +1,16 @@
 -- Todo
 return {
     {
+        --[[
+        -- Install instructions:
+            cd ~/.local/share/nvim/lazy
+            git clone https://github.com/iamcco/markdown-preview.nvim.git
+            cd markdown-preview.nvim
+            npx --yes yarn install
+            npx --yes yarn build
+        ]]
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
         config = function()
             vim.g.mkdp_filetypes = { "markdown" }
             vim.keymap.set('n', '<leader>mm', "<Plug>MarkdownPreview")
